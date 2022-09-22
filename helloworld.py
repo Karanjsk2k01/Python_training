5b23f89d25036ffab26c6a98b5948067bd04c5ee
unknown <karanvarun14501@gmail.com>
2022-09-22 23:44:47 +0530

    secod commit

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print('hello world')
+print('one')
+if 1 == 2:
+    print('two')
+print('three')
+print('four')
---
+++ variables.py
@@ -0,0 +1,15 @@
+
+# x = 10
+# y = 20
+
+# print(x)
+# print(z)
+
+
+x = "a"
+y = x
+z = "john"
+Z = "John"
+
+
+print("x =", x, "X = ", "Y =", "y =", y, "Z =", Z, "z =", z)
